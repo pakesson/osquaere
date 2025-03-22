e92aaacd8405165e77ac6ee122ff4c9e0d1b930f
Philip Åkesson <philip.akesson@gmail.com>
2025-03-22 14:12:57 +0100

    Apply APSW/SQLite best practices

--- osquaere/__main__.py
+++ osquaere/__main__.py
@@ -1,9 +1,13 @@
 import apsw
+import apsw.bestpractice
 
 from osquaere.modules import load_modules
 from osquaere.shell import Shell
 
+
 if __name__ == "__main__":
+    apsw.bestpractice.apply(apsw.bestpractice.recommended)
+
     db = apsw.Connection(":memory:")
     load_modules(db)
 
